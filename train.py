cb679b5f88a7cb221bca74060d2bf43843495e20
Anuj Jagannath Said <me21b172@smail.iitm.ac.in>
2025-03-17 20:16:32 +0530

    Chnaged defaults in train.py

--- train.py
+++ train.py
@@ -77,38 +77,38 @@ if __name__ == '__main__':
                         help="Wandb Entity used to track experiments in the Weights & Biases dashboard")
     parser.add_argument("-d", "--dataset", type=str, default="fashion_mnist", choices=["mnist", "fashion_mnist"],
                         help="Dataset to use")
-    parser.add_argument("-e", "--epochs", type=int, default=1,
+    parser.add_argument("-e", "--epochs", type=int, default=20,
                         help="Number of epochs to train neural network")
-    parser.add_argument("-b", "--batch_size", type=int, default=4,
+    parser.add_argument("-b", "--batch_size", type=int, default=64,
                         help="Batch size used to train neural network")
     parser.add_argument("-l", "--loss", type=str, default="cross_entropy",
                         choices=["mean_squared_error", "cross_entropy"],
                         help="Loss function to use")
-    parser.add_argument("-o", "--optimizer", type=str, default="sgd",
+    parser.add_argument("-o", "--optimizer", type=str, default="nadam",
                         choices=["sgd", "momentum", "nag", "rmsprop", "adam", "nadam"],
                         help="Optimizer to use")
-    parser.add_argument("-lr", "--learning_rate", type=float, default=0.1,
+    parser.add_argument("-lr", "--learning_rate", type=float, default=1e-4,
                         help="Learning rate used to optimize model parameters")
-    parser.add_argument("-m", "--momentum", type=float, default=0.5,
+    parser.add_argument("-m", "--momentum", type=float, default=0.99,
                         help="Momentum used by momentum and nag optimizers")
-    parser.add_argument("-beta", "--beta", type=float, default=0.5,
+    parser.add_argument("-beta", "--beta", type=float, default=0.99,
                         help="Beta used by rmsprop optimizer")
-    parser.add_argument("-beta1", "--beta1", type=float, default=0.5,
+    parser.add_argument("-beta1", "--beta1", type=float, default=0.99,
                         help="Beta1 used by adam and nadam optimizers")
-    parser.add_argument("-beta2", "--beta2", type=float, default=0.5,
+    parser.add_argument("-beta2", "--beta2", type=float, default=0.99,
                         help="Beta2 used by adam and nadam optimizers")
     parser.add_argument("-eps", "--epsilon", type=float, default=0.000001,
                         help="Epsilon used by optimizers")
-    parser.add_argument("-w_d", "--weight_decay", type=float, default=0.0,
+    parser.add_argument("-w_d", "--weight_decay", type=float, default=0.5,
                         help="Weight decay used by optimizers")
     parser.add_argument("-w_i", "--weight_init", type=str, default="Xavier",
                         choices=["random", "Xavier"],
                         help="Weight initialisation method")
-    parser.add_argument("-nhl", "--num_layers", type=int, default=1,
+    parser.add_argument("-nhl", "--num_layers", type=int, default=5,
                         help="Number of hidden layers used in feedforward neural network")
-    parser.add_argument("-sz", "--hidden_size", type=int, default=4,
+    parser.add_argument("-sz", "--hidden_size", type=int, default=252,
                         help="Number of hidden neurons in a feedforward layer")
-    parser.add_argument("-a", "--activation", type=str, default="sigmoid",
+    parser.add_argument("-a", "--activation", type=str, default="tanh",
                         choices=["identity", "sigmoid", "tanh", "ReLU"],
                         help="Activation function to use")
     
